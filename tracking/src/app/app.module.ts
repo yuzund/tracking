ed7d522c4e095979a5c50fa899cd3b284bf40437
yuzund <yuzund61298@gmail.com>
2020-05-05 14:35:10 +0900

    EDIT app.component.html

--- tracking/src/app/app.module.ts
+++ tracking/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatInputModule} from '@angular/material/input';
+import 'hammerjs';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +22,9 @@ import {MatCardModule} from '@angular/material/card';
     MatButtonModule,
     MatToolbarModule,
     MatCardModule,
-
+    MatIconModule,
+    MatRadioModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
